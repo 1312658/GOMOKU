ddaf6b76251d0de282e3b699d840553a70ba8110
1312658 <leanhtuan2802it@gmail.com>
2016-01-10 21:21:19 +0700

    Phien ban dau tien GOMOKU - Show toa do ban co

---
+++ MySomoku/1312658/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _1312658
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        String m_Message;
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+
+        private void btn_Chane_Click(object sender, RoutedEventArgs e)
+        {
+            m_Message += "\nSERVER\n Welcome ";
+            m_Message += txt_Your_Name.Text.ToString() + "!!!!\n" + DateTime.Now.ToString() + "\n -------------------------------------";
+            txt_Chat.Text = m_Message; 
+        }
+
+        private void btn_Send_Click(object sender, RoutedEventArgs e)
+        {
+            if (txt_Input_Chat.Text != "")
+            {
+                m_Message += "\n" + txt_Your_Name.Text.ToString() + ": " + txt_Input_Chat.Text.ToString() + "\n" + DateTime.Now.ToString() + "\n -------------------------------------";
+                txt_Input_Chat.Text = "";
+                txt_Chat.Text = m_Message;
+            }
+        }
+    }
+}
---
+++ MySomoku/1312658/My_Row.xaml.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _1312658
+{
+    /// <summary>
+    /// Interaction logic for My_Row.xaml
+    /// </summary>
+    public partial class My_Row : UserControl
+    {
+        int m_X;
+        int m_Y;
+
+        public int X
+        {
+                get { return m_X; }
+                set { m_X = value; }
+        }
+
+        public int Y
+        {
+            get { return m_Y; }
+            set { m_Y = value; }
+        }
+
+
+        public My_Row()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btn_C1_Y_Click(object sender, RoutedEventArgs e)
+        {
+            m_X = 1;
+            m_Y = Y;
+            MessageBox.Show(m_X.ToString() + ", " + m_Y.ToString());
+        }
+
+        private void btn_C2_Y_Click(object sender, RoutedEventArgs e)
+        {
+            m_X = 2;
+            m_Y = Y;
+            MessageBox.Show(m_X.ToString() + ", " + m_Y.ToString());
+        }
+
+        private void btn_C3_Y_Click(object sender, RoutedEventArgs e)
+        {
+            m_X = 3;
+            m_Y = Y;
+            MessageBox.Show(m_X.ToString() + ", " + m_Y.ToString());
+        }
+
+        private void btn_C4_Y_Click(object sender, RoutedEventArgs e)
+        {
+            m_X = 4;
+            m_Y = Y;
+            MessageBox.Show(m_X.ToString() + ", " + m_Y.ToString());
+        }
+
+        private void btn_C5_Y_Click(object sender, RoutedEventArgs e)
+        {
+            m_X = 5;
+            m_Y = Y;
+            MessageBox.Show(m_X.ToString() + ", " + m_Y.ToString());
+        }
+
+        private void btn_C6_Y_Click(object sender, RoutedEventArgs e)
+        {
+            m_X = 6;
+            m_Y = Y;
+            MessageBox.Show(m_X.ToString() + ", " + m_Y.ToString());
+        }
+
+        private void btn_C7_Y_Click(object sender, RoutedEventArgs e)
+        {
+            m_X = 7;
+            m_Y = Y;
+            MessageBox.Show(m_X.ToString() + ", " + m_Y.ToString());
+        }
+
+        private void btn_C8_Y_Click(object sender, RoutedEventArgs e)
+        {
+            m_X = 8;
+            m_Y = Y;
+            MessageBox.Show(m_X.ToString() + ", " + m_Y.ToString());
+        }
+
+        private void btn_C9_Y_Click(object sender, RoutedEventArgs e)
+        {
+            m_X = 9;
+            m_Y = Y;
+            MessageBox.Show(m_X.ToString() + ", " + m_Y.ToString());
+        }
+
+        private void btn_C10_Y_Click(object sender, RoutedEventArgs e)
+        {
+            m_X = 10;
+            m_Y = Y;
+            MessageBox.Show(m_X.ToString() + ", " + m_Y.ToString());
+        }
+
+        private void btn_C11_Y_Click(object sender, RoutedEventArgs e)
+        {
+            m_X = 11;
+            m_Y = Y;
+            MessageBox.Show(m_X.ToString() + ", " + m_Y.ToString());
+        }
+
+        private void btn_C12_Y_Click(object sender, RoutedEventArgs e)
+        {
+            m_X = 12;
+            m_Y = Y;
+            MessageBox.Show(m_X.ToString() + ", " + m_Y.ToString());
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (Y % 2 == 1)
+            {
+                btn_C1_Y.Background = Brushes.DarkGray;
+                btn_C3_Y.Background = Brushes.DarkGray;
+                btn_C5_Y.Background = Brushes.DarkGray;
+                btn_C7_Y.Background = Brushes.DarkGray;
+                btn_C9_Y.Background = Brushes.DarkGray;
+                btn_C11_Y.Background = Brushes.DarkGray;
+
+                btn_C2_Y.Background = Brushes.White;
+                btn_C4_Y.Background = Brushes.White;
+                btn_C6_Y.Background = Brushes.White;
+                btn_C8_Y.Background = Brushes.White;
+                btn_C10_Y.Background = Brushes.White;
+                btn_C12_Y.Background = Brushes.White;
+            }
+            if (Y % 2 == 0)
+            {
+                btn_C2_Y.Background = Brushes.DarkGray;
+                btn_C4_Y.Background = Brushes.DarkGray;
+                btn_C6_Y.Background = Brushes.DarkGray;
+                btn_C8_Y.Background = Brushes.DarkGray;
+                btn_C10_Y.Background = Brushes.DarkGray;
+                btn_C12_Y.Background = Brushes.DarkGray;
+
+                btn_C1_Y.Background = Brushes.White;
+                btn_C3_Y.Background = Brushes.White;
+                btn_C5_Y.Background = Brushes.White;
+                btn_C7_Y.Background = Brushes.White;
+                btn_C9_Y.Background = Brushes.White;
+                btn_C11_Y.Background = Brushes.White;
+            }
+        }
+    }
+}
